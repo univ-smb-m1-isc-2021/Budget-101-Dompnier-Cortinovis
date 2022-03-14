fa51248809cfba1a55a9af3f3e8cd17a389ba688
Magira73 <silvio_dompnier@hotmail.fr>
2022-03-14 16:59:05 +0100

    spring-boot-init

---
+++ budget101/src/main/java/com/example/budget101/Budget101Application.java
@@ -0,0 +1,13 @@
+package com.example.budget101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Budget101Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Budget101Application.class, args);
+	}
+
+}
---
+++ budget101/src/test/java/com/example/budget101/Budget101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.budget101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Budget101ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
