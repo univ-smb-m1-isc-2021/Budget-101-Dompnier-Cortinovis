f22d492f0522f478e2bbf893c984f50610cc9713
Magira73 <silvio_dompnier@hotmail.fr>
2022-03-24 14:48:36 +0100

    Update Budget101Application.java

--- budget101/src/main/java/com/example/budget101/Budget101Application.java
+++ budget101/src/main/java/com/example/budget101/Budget101Application.java
@@ -1,6 +1,5 @@
 package com.example.budget101;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
