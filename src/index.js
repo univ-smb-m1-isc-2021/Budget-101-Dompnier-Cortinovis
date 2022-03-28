cffd87a5f21e005c882a11b7b629ff87f9739d5e
Enzo <bowss.mail@gmail.com>
2022-03-28 15:30:01 +0200

    start test

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ import Auth from "layouts/Auth.js";
 import Landing from "views/Landing.js";
 import Profile from "views/Profile.js";
 import Index from "views/Index.js";
+import Login from "views/auth/Login.js";
 
 ReactDOM.render(
   <BrowserRouter>
@@ -25,7 +26,7 @@ ReactDOM.render(
       {/* add routes without layouts */}
       <Route path="/landing" exact component={Landing} />
       <Route path="/profile" exact component={Profile} />
-      <Route path="/" exact component={Index} />
+      <Route path="/" exact component={Login} />
       {/* add redirect for first page */}
       <Redirect from="*" to="/" />
     </Switch>
