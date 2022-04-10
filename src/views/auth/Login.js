2a066c717526d98978ab19b0d531c64c98e8abab
Enzo <bowss.mail@gmail.com>
2022-04-10 20:08:43 +0200

    cleaning

--- src/views/auth/Login.js
+++ src/views/auth/Login.js
@@ -7,13 +7,7 @@ export default class Login extends React.Component {
   constructor(props) {
     super(props);
     this.state = { value: '' };
-
-    this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({ value: event.target.value });
   }
 
   handleSubmit(event) {
